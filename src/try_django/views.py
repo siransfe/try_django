9a7f1092787e997f3d27e7f26238ea7ee4a658e1
siransfe <hclee8907@gmail.com>
2020-09-25 15:03:22 +0900

    Recover my project because of my error in my foler trees

---
+++ src/try_django/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+
+def home_page(request):
+    my_title    = "Hello there..."
+    context     = {"title": my_title, "my_list" : [1, 2, 3, 4, 5]}
+    return render(request, "home.html", context)
+
+def about_page(request):
+    return HttpResponse("<h1>helloWorld</h1>")
+
+def contact_page(request):
+    return render(request, "hello_world.html", {"title": "Contact us" })
+
+
+def example_page(request):
+    context         = {"title": my_title}
+    template_name   = "hello_world.html"
+    template_obj    = get_template(template_name)
+    rendered_item   = template_obj.render(context)
+    return HttpResponse(rendered_item)
