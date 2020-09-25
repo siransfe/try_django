2f9dddfec5efe36a1a7d050aba0beccd66529bd8
siransfe <hclee8907@gmail.com>
2020-09-25 15:21:10 +0900

    Create another app

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 def home_page(request):
-    my_title    = "Hello there..."
-    context     = {"title": my_title, "my_list" : [1, 2, 3, 4, 5]}
+    my_title = "Hello there..."
+    context = {"title": "my_title"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list" : [1, 2, 3, 4, 5]}
     return render(request, "home.html", context)
 
 def about_page(request):
