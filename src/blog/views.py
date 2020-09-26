2681778f904328aba82aa8e0099e76f3c4f62f12
siransfe <hclee8907@gmail.com>
2020-09-26 16:05:18 +0900

    Correct Error > objets > objects.

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,8 +7,17 @@ from .models import BlogPost
 #get -> 1 object
 #filter -> [] objects
 def blog_post_detail_page (request, slug):
-    queryset = BlogPost.objets.filter(slug = slug)
+    '''
+    queryset = BlogPost.objects.filter(slug = slug)
+    if queryset.count() != 1:
+        obj = queryset.first()
+    '''
+    queryset = BlogPost.objects.filter(slug = slug)
+    if queryset.count() != 1:
+        obj = queryset.last()
     #obj = get_object_or_404(BlogPost, id = id)
+    else:
+        obj = BlogPost.objects.get(slug=slug)
     template_name = "blog_post_detail_page.html"
     context = {"object": obj}
     return render(request, template_name, context)
