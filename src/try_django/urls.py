ae01408067eb78cf96b40d02466b157d91ae67c7
siransfe <hclee8907@gmail.com>
2020-09-26 16:53:07 +0900

    Add Include function in urls.

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -1,27 +1,16 @@
-"""try_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home_page, about_page, contact_page, example_page
-from blog.views import blog_post_detail_page, blog_post_list_view
+from blog.views import blog_post_create_view
+
+
 urlpatterns = [
     path('', home_page),
+    
+    path('blog/', include('blog.urls')),
+    path('blog-new/',  blog_post_create_view),
+]
     path('about/', about_page),
     path('contact/', home_page),
-    path('blog/<str:slug>/', blog_post_detail_page),
-    path('blog/', blog_post_list_view),
     path('admin/', admin.site.urls),
-]
+    
