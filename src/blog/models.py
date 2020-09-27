43f0438e90f81950f11cc760f8e429276211341b
siransfe <hclee8907@gmail.com>
2020-09-27 17:28:50 +0900

    Convert form to ModelForm New migrations

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
-    title   = models.TextField()
+    title   = models.CharField(max_length = 100)
     slug    = models.SlugField(unique =True) #hello world -> hello-world
     content = models.TextField(null = True, blank=True)
     
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .forms import BlogPostForm
+from .forms import BlogPostForm, BlogPostModelForm
 from .models import BlogPost
 
 #get -> 1 object
@@ -30,12 +30,15 @@ def blog_post_list_view (request):
     return render(request, template_name, context)
 
 def blog_post_create_view(request):
-    form = BlogPostForm(request.POST or None)
+    #form = BlogPostForm(request.POST or None)
+    form = BlogPostModelForm(request.POST or None)
     if form.is_valid():
-        print(form.cleaned_data) #dic datas
-        title = form.cleaned_data['title']
-        obj = BlogPost.objects.create(**form.cleaned_data)
-        obj = BlogPostForm()
+        #print(form.cleaned_data) #dic datas
+        form.save()
+        form = BlogPostModelForm()
+        #title = form.cleaned_data['title']
+        #obj = BlogPost.objects.create(**form.cleaned_data)
+        #obj = BlogPostForm()
     template_name = 'form.html'
     context = {'form': form}
     return render(request, template_name, context)
