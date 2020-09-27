3123a9bead2160326fc1f1c838124d7eed9e4f56
siransfe <hclee8907@gmail.com>
2020-09-27 17:48:11 +0900

    Create Check func in forms.

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -13,3 +13,12 @@ class BlogPostModelForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'slug', 'content'] #what you want to input
+        
+    
+    def clean_title(self, *args, **kwagrs):
+        title = self.cleaned_data.get('title')
+        qs =BlogPost.objects.filter(title__iexact = title)##iexact를 쓰면 대문자 소문자 전부 같은걸로 취급.
+        print(title)
+        if qs.exists():
+            raise forms.ValidationError("This title has already been used. please try another title name")
+        return title
--- src/try_django/forms.py
+++ src/try_django/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
-
 class ContactForm(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     content = forms.CharField(widget= forms.Textarea)
+    
+    
+    def clean_email(self, *args, **kwargs):
+        print(self.cleaned_data)
+        email = self.cleaned_data.get('email')
+        if email.endswith(".edu"):
+            raise forms. ValidationError("This is not valid email format. please don't user that")
+        return email
     
