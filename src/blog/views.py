741bc8da1dd2420af643b291e9badea744cc5c15
siransfe <hclee8907@gmail.com>
2020-09-28 12:26:50 +0900

    Modify delete view and put redirect method

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .forms import BlogPostForm, BlogPostModelForm
@@ -58,6 +58,7 @@ def blog_post_detail_view (request, slug):
     context = {"object": obj}
     return render(request, template_name, context)
 
+@staff_member_required
 def blog_post_update_view (request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     form = BlogPostModelForm(request.POST or None, instance = obj)
@@ -68,9 +69,13 @@ def blog_post_update_view (request, slug):
     context = {'form':form, "title": f"Update {obj.title}"}
     return render(request, template_name, context)
 
-
+@staff_member_required
 def blog_post_delete_view (request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "form.html"
+    template_name = "blog/delete.html"
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
+
     context = {"object": obj}
     return render(request, template_name, context)
